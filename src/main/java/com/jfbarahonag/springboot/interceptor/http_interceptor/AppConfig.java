c1ef9524d0fba3a09c77dfdce11a50ffa1c5daf6
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-23 08:00:59 -0500

    app config has been created to register the LoadingTimeInterceptor to the app

---
+++ src/main/java/com/jfbarahonag/springboot/interceptor/http_interceptor/AppConfig.java
@@ -0,0 +1,22 @@
+package com.jfbarahonag.springboot.interceptor.http_interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+
+  @Autowired
+  @Qualifier("ltInterceptor")
+  private HandlerInterceptor timeInterceptor;
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(timeInterceptor);
+  }
+  
+}
--- src/main/java/com/jfbarahonag/springboot/interceptor/http_interceptor/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/com/jfbarahonag/springboot/interceptor/http_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -1,5 +1,7 @@
 package com.jfbarahonag.springboot.interceptor.http_interceptor.interceptors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,19 +9,21 @@ import org.springframework.web.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@Component
+@Component("ltInterceptor")
 public class LoadingTimeInterceptor implements HandlerInterceptor {
+
+  private static final Logger logger = LoggerFactory.getLogger(LoadingTimeInterceptor.class);
+  
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    logger.info("LoadingTimeInterceptor: preHandle()");
+    return false;
+  }
 
   @Override
   public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
       ModelAndView modelAndView) throws Exception {
-        
+        logger.info("LoadingTimeInterceptor: postHandle()");
   }
-
-  @Override
-  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-    return false;
-  }
-  
 
 }
